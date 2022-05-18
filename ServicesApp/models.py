48a2ae2ed8c2dea43559f848c0da26f9cae21027
Esteban Velasquez <eavc.30@gmail.com>
2022-05-17 19:07:07 -0500

    feat(back): upload service images
    - add upload_to property in the model.
    -  add paths for static and media files.
    - add variables for the media url and its root.

--- ServicesApp/models.py
+++ ServicesApp/models.py
@@ -8,7 +8,7 @@ class Service(models.Model):
         max_length=200, verbose_name='Name', unique=True, null=False)
     description = models.TextField(verbose_name='Description', null=False)
     icon = models.CharField(max_length=200, verbose_name='Icon', null=False)
-    image = models.ImageField(verbose_name='Image', null=False)
+    image = models.ImageField(verbose_name='Image', upload_to='services/', null=False)
     created = models.DateTimeField(auto_now_add=True, verbose_name='Created')
     updated = models.DateTimeField(auto_now=True, verbose_name='Updated')
 
--- WebAppWithDjango/urls.py
+++ WebAppWithDjango/urls.py
@@ -3,6 +3,8 @@
 The `urlpatterns` list routes URLs to views.
 """
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from WebAppWithDjango import views
 
@@ -13,3 +15,6 @@ urlpatterns = [
     path('blog/', views.blog, name='blog'),
     path('contact/', views.contact, name='contact'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
